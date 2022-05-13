021675b27d220744c416cd3976ea57887ffe0924
Mauricio Teruel <mauricioteruel98@gmail.com>
2022-05-12 22:26:27 -0300

    Changes in database file

--- backend/database.sql
+++ backend/database.sql
@@ -1,13 +1,9 @@
-CREATE DATABASE budget
-    DEFAULT CHARACTER SET = 'utf8mb4';
+CREATE DATABASE budgetapp
 
 CREATE TABLE fact(  
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
-    concept VARCHAR(255) NOT NULL COMMENT 'concept',
-    amount VARCHAR(255) NOT NULL COMMENT 'amount',
-    type VARCHAR(255) NOT NULL COMMENT 'type',
-    create_time DATETIME NOT NULL COMMENT 'Create Time',
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    concept VARCHAR(255) NOT NULL,
+    amount VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    create_time DATETIME NOT NULL
 )ENGINE=InnoDb;
-
-
-INSERT INTO fact VALUES(NULL, 'Sueldo', '1.200', 'ingress', CURTIME());
